bbae995a4a2309f15858e776b63f8bc449b89c78
inthahouse <alan.nguy@outlook.com>
2021-01-17 22:23:40 -0500

    env fix

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,5 +5,7 @@ const CLIENT_ID = 'ef06f6883920433ea7310fd0d616b386'
 export const AUTH_SUCCESS_STATE = 'AUTH_SUCCESS_STATE'
 export const BASE_API = 'https://api.spotify.com/v1'
 export const SPOTIFY_AUTH_ROUTE = `${AUTH_END_POINT}?client_id=${CLIENT_ID}&redirect_uri=${
-  process.env.REDIRECT_URI ? process.env.REDIRECT_URI : REDIRECT_URI
+  process.env.NODE_ENV === 'development'
+    ? REDIRECT_URI
+    : process.env.REDIRECT_URI
 }&scope=user-read-private&response_type=token&state=${AUTH_SUCCESS_STATE}`
